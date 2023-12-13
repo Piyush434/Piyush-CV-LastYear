5866143e4f231e2b5663b188d3525333982de1f4
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 22:54:29 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from skimage import color
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def f(img):
+    # splitting image into color channels
+    R = img[:,:,0]
+    G = img[:,:,1]
+    B = img[:,:,2]
+    # calculating mean values for each of the color channel along vertical axis
+    Rm=np.mean(R,axis=0)
+    rm1=np.mean(Rm);
+    Gm=np.mean(G,axis=0)
+    gm1=np.mean(Gm);
+    Bm=np.mean(B,axis=0);
+    bm1=np.mean(Bm);
+    # Color balancing
+    Irc = np.double(R)+np.double((gm1-rm1))*np.double((1-rm1))*np.double(G)
+    st.image(Irc)
