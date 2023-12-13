57b3fbdbaa07574f70d0f940ee41ffd66c19bedf
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 23:16:22 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -6,20 +6,3 @@ import matplotlib.pyplot as plt
 
 def f(img):
     st.text('heere is after helper')
-    # splitting image into color channels
-    R = img[:,:,0]
-    G = img[:,:,1]
-    B = img[:,:,2]
-    # calculating mean values for each of the color channel along vertical axis
-    Rm=np.mean(R,axis=0)
-    rm1=np.mean(Rm);
-    Gm=np.mean(G,axis=0)
-    gm1=np.mean(Gm);
-    Bm=np.mean(B,axis=0);
-    bm1=np.mean(Bm);
-    # Color balancing
-    Irc = np.double(R)+np.double((gm1-rm1))*np.double((1-rm1))*np.double(G)
-    plt.imshow(Irc)
-    plt.savefig('test',dpi=400)
-    st.image('test.png')
-    os.remove('test.png')
