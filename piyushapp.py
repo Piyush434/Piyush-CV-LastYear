30ca2827d17544f2604540dc99dde3e1f2ff520d
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 22:54:58 +0530

    Update piyushapp.py

--- piyushapp.py
+++ piyushapp.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import helper
 
 img_main = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
 if img_main is not None:
     image = Image.open(img_main)
     img_main = np.array(image)
     st.image(img_main)
+    helper.f(img_main)
 else:
     st.subheader(':heavy_exclamation_mark: Please upload the image')
