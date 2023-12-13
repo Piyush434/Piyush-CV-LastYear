c11f696bfd459c17b3986cf0c6d34c65e74892b7
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 21:47:17 +0530

    Update piyushapp.py

--- piyushapp.py
+++ piyushapp.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import cv2
-img_main= cv2.imread(st.file_uploader("Add image ",type=['png', 'jpg', 'jpeg']))
-var = type(img_main)
-st.text(var)  
-                                              
+import numpy as np
+from PIL import Image
+
+img_file_buffer = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image)
