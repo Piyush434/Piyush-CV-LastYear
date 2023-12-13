c2035c0322e6a6396416c88ac846151aec2fd129
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 21:50:10 +0530

    Update piyushapp.py

--- piyushapp.py
+++ piyushapp.py
@@ -4,6 +4,6 @@ from PIL import Image
 
 img_main = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
 if img_main is not None:
-    image = Image.open(img_file_buffer)
+    image = Image.open(img_main)
     img_main = np.array(image)
 st.text(type(img_main))
