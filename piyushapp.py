6e5a0c485fbcb644d358b7dc46e66e195ea859cf
PIYUSH LAXMAN BHONDAVE <87574036+Piyush434@users.noreply.github.com>
2023-12-13 21:48:39 +0530

    Update piyushapp.py

--- piyushapp.py
+++ piyushapp.py
@@ -2,7 +2,8 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-img_file_buffer = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
-if img_file_buffer is not None:
+img_main = st.file_uploader('Upload a PNG image', type=['png', 'jpg', 'jpeg'])
+if img_main is not None:
     image = Image.open(img_file_buffer)
-    img_array = np.array(image)
+    img_main = np.array(image)
+st.text(type(img_main))
